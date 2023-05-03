4f93709b66f891dde6a8f6966d3e350cc090ce65
Will Liu <will990211@163.com>
2023-05-03 19:51:38 -0400

    basic complete

--- cmd/DBFS/namenode/main.go
+++ cmd/DBFS/namenode/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	node "dfs/internal/node"
+	"time"
+)
 
+func main() {
+	n := node.MakeNameNode()
+
+	for n.Done() == false {
+		time.Sleep(time.Second)
+	}
 }
